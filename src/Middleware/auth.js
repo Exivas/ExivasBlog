bba956862cb81f26f41b2db0e0c178469aac1f83
Exivas <41807974+Exivas@users.noreply.github.com>
2024-12-06 20:42:49 -0400

    Update auth.js

--- src/Middleware/auth.js
+++ src/Middleware/auth.js
@@ -1,21 +1,25 @@
 import jwt from 'jsonwebtoken';
 
 export const auth = (req, res, next) => {
-    const token =req.headers('authorization');
-    if(!token){
-        return res.status(401).json({message: "Unauthorized"})
+    // Accede al encabezado de autorización correctamente
+    const token = req.headers.authorization?.split(' ')[1];  // 'Bearer token'
+    
+    if (!token) {
+        return res.status(401).json({ message: "Unauthorized" });
     }
+
     try {
         const decoded = jwt.verify(token, process.env.KEY);
-        req.user = decoded;
-        next();
+        req.user = decoded;  // Almacenar los datos del usuario decodificados
+        next();  // Continuar con la siguiente función
     } catch (error) {
-        return res.status(401).json({message: "Unauthorized"})
-}}
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+}
 
 export const isAdmin = (req, res, next) => {
-    if(req.user.role !== 'admin'){
-        return res.status(401).json({message: "Unauthorized"})
+    if (req.user.role !== 'admin') {
+        return res.status(401).json({ message: "Unauthorized" });
     }
-    next();
+    next();  // Continuar si el usuario es admin
 }
