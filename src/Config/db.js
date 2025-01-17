337a4109529f503f2ea05b03595281f1ace0b669
Exivas <41807974+Exivas@users.noreply.github.com>
2025-01-17 15:48:40 -0400

    Update db.js

--- src/Config/db.js
+++ src/Config/db.js
@@ -1,13 +1,24 @@
 import { Sequelize } from "sequelize";
 
+const isRemote = process.env.DATABASE_URL && process.env.DATABASE_URL !== '';
 
-// Crear la conexión a la base de datos
-const connect = new Sequelize({
-  dialect: 'postgres',
-  host: 'localhost',
-  username: process.env.USER,
-  password: process.env.PASS,
-  database: process.env.DATABASE
-});
+const connect = isRemote
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      protocol: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      }
+    })
+  : new Sequelize({
+      dialect: 'postgres',
+      host: 'localhost',
+      username: process.env.USER,
+      password: process.env.PASS,
+      database: process.env.DATABASE
+    });
 
 export default connect;
